f7fd94b798e6f57f62c8511f2a39945dcfd469a0
nata44845 <krppost@yandex.ru>
2023-01-17 22:02:14 +0700

    homework

---
+++ task6_even/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a%2==0) Console.WriteLine("да");
+else Console.WriteLine("нет");
---
+++ task8_evenN/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+int i=2;
+while (i<N)
+{
+    Console.Write(i);
+    if (i<N && i+1<N) Console.Write(", ");
+    i+=2;
+}
+
+if (N>1 && N%2==0) Console.WriteLine(N);
